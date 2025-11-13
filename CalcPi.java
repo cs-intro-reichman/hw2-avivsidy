7cc0fa47be43843d15c736e99f5e63f90ec687c2
Aviv Sidy <aviv.sidy@post.runi.ac.il>
2025-11-13 22:37:05 +0200

    Completed assignment

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,18 @@
 // Computes an approximation of PI.
-public class CalcPi {
-	public static void main(String [] args) { 
-	    // Replace this comment with your code
-	}
+public class CalcPi
+{
+	public static void main(String [] args)
+	{ 
+	    int sign=-1;
+		double piApproximation=1.0;
+		int numOfTerms=Integer.parseInt(args[0]);
+		for (int i=0; i<numOfTerms-1; i++)
+		{
+			piApproximation += sign/(2.0*i+3.0);
+			sign = -sign;
+		}
+		piApproximation = piApproximation * 4.0;
+		System.err.println("pi according to java: " + Math.PI);
+		System.err.println("pi, approximated:     " + piApproximation);
+}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,6 +1,48 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
-	    // Replace this comment with your code
-	}
+	    int N=Integer.parseInt(args[0]);
+		int seed;
+		String mode=args[1];
+		int countIterations=1;
+		for (int i=1; i<=N; i++)
+		{
+			seed=i;
+			if (seed==1) 
+			{
+				if (mode.equals("v"))
+				{
+					countIterations++;
+					System.err.print(seed + " ");
+				}
+				seed=3*seed+1;
+			}
+			while (seed!=1)
+			{
+				if (mode.equals("v"))
+				{
+		    		System.out.print(seed + " ");
+				}
+				if (seed%2==0) {
+			    	seed=seed/2;
+				} 
+				else
+				{
+			    	seed=3*seed+1;
+				}
+				countIterations++;
+			}
+			if (mode.equals("v"))
+			{
+				System.err.print("1 " + "(" + countIterations + ")");
+				System.err.println();
+				countIterations=1;
+			}	
+		}
+		if (mode.equals("v") || mode.equals("c"))
+		{
+			System.err.println();		
+			System.err.println("Every one of the first " + N + " hailstone sequences reached 1.");
+		}	
+}
 }
